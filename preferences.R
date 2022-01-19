6074d4b57e4279cf4968b108d7cd5feb314fa9fa
mpiorczynski <m.piorczynski13@gmail.com>
2022-01-20 00:12:38 +0100

    Final

--- preferences.R
+++ preferences.R
@@ -239,6 +239,7 @@ my_theme <- bs_theme(
 )
 # ------------------------------------------------------------------------------
 
+
 server <- function(input, output, session){
   
   
@@ -344,7 +345,7 @@ server <- function(input, output, session){
     max_time <- max(df$ts)
     
     tagList(
-      h5("Select time period:"),
+      h5("Select time period:", style = "color: #FFFFFF;"),
       sliderInput(
         inputId = "p2_time",
         label = NULL,
@@ -482,7 +483,7 @@ server <- function(input, output, session){
     
     p2_ed <- event_data("plotly_click", source="p2_comp")
     if(is.null(p2_ed)) { tagList(
-      h4("What time of the day do we listen to music?"),
+      h4("What time of the day do we listen to music?", style = "color: #FFFFFF;"),
       withSpinner(plotlyOutput("p2_density"), color="#1ED760", type=4),
       uiOutput("p2_UI_time_input") 
     )}
@@ -499,7 +500,7 @@ server <- function(input, output, session){
           ),
           
           box(width = 6,
-              h4(paste("What time of the week does", p2_person, "listen to music?")),
+              h4(paste("What time of the week does", p2_person, "listen to music?"), style = "color: #FFFFFF;"),
               withSpinner(plotlyOutput("p2_heatmap"), color="#1ED760", type=4),
               background = "red"
           )
@@ -538,7 +539,7 @@ server <- function(input, output, session){
         ),
         
         fluidRow(
-          h4(paste(p2_person, "'s listening time last year", sep="")),
+          h4(paste(p2_person, "'s listening time last year", sep=""), style = "color: #FFFFFF;"),
           withSpinner(plotlyOutput("p2_bar"), color="#1ED760", type=4)
         )
       )
@@ -794,6 +795,251 @@ server <- function(input, output, session){
       config(displayModeBar = FALSE)
     
   })
+  
+  
+  output$plot3 <- plotly::renderPlotly({
+    
+    if(input$personPlot3 == "Mikołaj"){
+      df <- df_mikolaj
+    }
+    else if(input$personPlot3 == "Krzysiek"){
+      df <- df_krzysiek
+    }
+    else{
+      df <- df_daniel
+    }
+    
+    
+    
+    df1 <- df %>% 
+      group_by(master_metadata_album_artist_name) %>% 
+      summarise(Time = sum(ms_played)/60000) %>% 
+      arrange(-Time) %>% 
+      head(input$n) %>% 
+      mutate(master_metadata_album_artist_name = fct_reorder(master_metadata_album_artist_name, Time))
+    
+    
+    p <- ggplot(df1,aes(x = Time, y = master_metadata_album_artist_name))+
+      geom_col(fill = "#1ED760")+
+      theme(panel.background = element_rect(fill = "#444444"),
+            plot.background = element_rect(fill = "#444444"),
+            text = element_text(color = "#FFFFFF"),
+            axis.text = element_text(color = "#FFFFFF"),
+            legend.text = element_text(colour = "#FFFFFF"),
+            axis.ticks = element_blank(),
+            panel.grid.minor = element_blank(),
+            panel.grid.major = element_line(size = 0.3, colour = "#888888"),
+            axis.title.y = element_blank(),
+            axis.title.x = element_text())+
+      labs(title = "Favourite Artists", x = "Minutes listened")
+    
+    plotly::ggplotly(p, source = "1")
+  })
+  
+  output$plot4 <- plotly::renderPlotly({
+    
+    
+    if(input$personPlot3 == "Mikołaj"){
+      df <- df_mikolaj
+    }
+    else if(input$personPlot3 == "Krzysiek"){
+      df <- df_krzysiek
+    }
+    else {
+      df <- df_daniel
+    }
+    
+    
+    
+    df1 <- df %>% 
+      group_by(master_metadata_album_album_name) %>% 
+      summarise(Time = sum(ms_played)/60000) %>% 
+      arrange(-Time) %>% 
+      head(input$n) %>% 
+      mutate(master_metadata_album_album_name = fct_reorder(master_metadata_album_album_name, Time))
+    
+    
+    p <- ggplot(df1,aes(x = Time, y = master_metadata_album_album_name))+
+      geom_col(fill = "#1ED760")+
+      theme(panel.background = element_rect(fill = "#444444"),
+            plot.background = element_rect(fill = "#444444"),
+            text = element_text(color = "#FFFFFF"),
+            axis.text = element_text(color = "#FFFFFF"),
+            legend.text = element_text(colour = "#FFFFFF"),
+            axis.ticks = element_blank(),
+            panel.grid.minor = element_blank(),
+            panel.grid.major = element_line(size = 0.3, colour = "#888888"),
+            axis.title.y = element_blank(),
+            axis.title.x = element_text())+
+      labs(title = "Favourite albums", x = "Minutes listened")
+    
+    plotly::ggplotly(p, source = "2")
+  })
+  
+  observeEvent(event_data("plotly_click", source = "2"), {
+    if(input$id=="Plot3"){
+      
+      if(input$personPlot3 == "Mikolaj"){
+        df <- df_mikolaj
+      }
+      else if(input$personPlot3 == "Krzysiek"){
+        df <- df_krzysiek
+      }
+      else{
+        df <- df_daniel
+      }
+      
+      
+      barData = event_data("plotly_click", source = "2")
+      
+      
+      df1 <- df %>% 
+        group_by(master_metadata_album_album_name) %>% 
+        summarise(Time = sum(ms_played)/60000) %>% 
+        arrange(-Time) %>% 
+        head(input$n) %>% 
+        mutate(master_metadata_album_album_name = fct_reorder(master_metadata_album_album_name, Time))
+      
+      artist <- df1[barData$pointNumber+1,1] %>% 
+        mutate(master_metadata_album_album_name = as.character(master_metadata_album_album_name))
+      
+      artist <- as.character(artist)
+      
+      favSong <- df %>% 
+        filter(master_metadata_album_album_name == artist) %>% 
+        group_by(master_metadata_track_name) %>% 
+        summarise(Sum = sum(ms_played)/60000) %>%
+        arrange(-Sum) %>% 
+        head(5)
+      colnames(favSong) <- c("Track Name", "Minutes Listened")
+      
+      
+      
+      
+      
+      showModal(modalDialog(title = , easyClose = TRUE,
+                            render_gt({
+                              gt(favSong) %>% 
+                                tab_header(title = md("Favourite tracks on selected album")) %>% 
+                                tab_style(
+                                  style = list(
+                                    cell_text(color = 'white'),
+                                    cell_fill(color = "#444444")
+                                  ),
+                                  locations = list(
+                                    cells_body(),
+                                    cells_title(),
+                                    cells_column_labels()
+                                    
+                                  )
+                                )
+                              
+                              
+                            })
+      ))
+    }
+    
+  })
+  
+  
+  
+  
+  observeEvent(event_data("plotly_click", source = "1"), {
+    if(input$id == "Plot3"){
+      
+      if(input$personPlot3 == "Mikolaj"){
+        df <- df_mikolaj
+      }
+      else if(input$personPlot3 == "Krzysiek"){
+        df <- df_krzysiek
+      }
+      else{
+        df <- df_daniel
+      }
+      
+      barData = event_data("plotly_click", source = "1")
+      
+      df1 <- df %>% 
+        group_by(master_metadata_album_artist_name) %>% 
+        summarise(Time = sum(ms_played)/60000) %>% 
+        arrange(-Time) %>% 
+        head(input$n) %>% 
+        mutate(master_metadata_album_artist_name = fct_reorder(master_metadata_album_artist_name, Time))
+      
+      artist <- df1[barData$pointNumber+1,1] %>% 
+        mutate(master_metadata_album_artist_name = as.character(master_metadata_album_artist_name))
+      
+      artist <- as.character(artist)
+      
+      favSong <- df %>% 
+        filter(master_metadata_album_artist_name == artist) %>% 
+        group_by(master_metadata_track_name) %>% 
+        summarise(Sum = sum(ms_played)/60000) %>%
+        arrange(-Sum) %>% 
+        head(5)
+      colnames(favSong) <- c("Track Name", "Minutes Listened")
+      
+      
+      
+      showModal(modalDialog(easyClose = TRUE, title = tags$a(style = "color: white", icon('robot'), br(), as.character(artist)), renderPlot({
+        
+        endMonth <- as.character(c("01","02","03","04","05","06","07","08","09","10","11","12"))
+        MonthSum <- c(0,0,0,0,0,0,0,0,0,0,0,0)
+        
+        df1 <- data.frame(endMonth, MonthSum)
+        
+        
+        
+        df2 <- df %>% 
+          mutate(endTime = as.Date(ts)) %>% 
+          mutate(
+            endMonth = strftime(endTime, "%m")
+          ) %>% 
+          filter(master_metadata_album_artist_name == artist) %>% 
+          group_by(endMonth) %>% 
+          summarise(MonthSum = sum(ms_played)/60000)
+        
+        df3 <- rbind(df2,df1)
+        
+        ggplot(df3,aes(x = endMonth, y = MonthSum))+
+          geom_col(fill = "#1ED760")+
+          theme(panel.background = element_rect(fill = "#444444"),
+                plot.background = element_rect(fill = "#444444"),
+                text = element_text(color = "#FFFFFF"),
+                axis.text = element_text(color = "#FFFFFF"),
+                legend.text = element_text(colour = "#FFFFFF"),
+                axis.ticks = element_blank(),
+                panel.grid.minor = element_blank(),
+                panel.grid.major = element_line(size = 0.3, colour = "#888888"),
+                axis.title.y = element_blank(),
+                axis.title.x = element_text())+
+          labs(x = "Month", y = "Minutes played")
+      }),
+      
+      br(),
+      render_gt({
+        gt(favSong) %>% 
+          tab_header(title = md("Favourite tracks")) %>% 
+          tab_style(
+            style = list(
+              cell_text(color = 'white'),
+              cell_fill(color = "#444444")
+            ),
+            locations = list(
+              cells_body(),
+              cells_title(),
+              cells_column_labels()
+              
+            )
+          )
+        
+        
+      })
+      
+      ))
+    }
+  })
+  
 
 }
   
@@ -888,15 +1134,15 @@ app_ui <- dashboardPage(
       }
       
       .fa .fa-backward {
-        background-color: #040404
+        background-color: #040404;
       }
       
       #p2_buckets {
-        background-color: #040404
+        background-color: #040404;
       }
       
       .form-group .shiny-input-radiogroup .shiny-input-container .shiny-input-container-inline .shiny-bound-input .shinyjs-resettable {
-        background-color: #040404
+        background-color: #040404;
       }
 
     '))),
@@ -913,7 +1159,11 @@ app_ui <- dashboardPage(
               
       ),
       tabItem(tabName = "top",
-              h2("No tab content")
+              chooseSliderSkin("Flat", "#1ED760"),
+              fluidRow(
+                box(plotlyOutput("plot3")),
+                box(plotlyOutput("plot4"))
+              )
       ),
       tabItem(tabName = "activity", 
               useShinyjs(),
@@ -926,4 +1176,3 @@ app_ui <- dashboardPage(
 )
 
 shinyApp(app_ui, server)
-
